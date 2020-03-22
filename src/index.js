8d18e6cb72079e578c2bc414ca273ff308f00972
HelgaAnt <antou@bk.ru>
2020-03-22 19:02:52 +0400

    init: start brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  bracketsConfig = bracketsConfig.join(',');
+  bracketsConfig = bracketsConfig.split(',');
+  let arr = [];
+  let temp = '';
+  let tt =  '';
+   
+  for (let i = 0; i < bracketsConfig.length; i = i + 2){
+  
+    if ( bracketsConfig[i] == '(' || bracketsConfig[i] == '[' || bracketsConfig[i] == '{' || bracketsConfig[i] == '|' )  
+      tt = '\\' + bracketsConfig[i] + '\\' +  bracketsConfig[i+1];
+    else
+      tt = bracketsConfig[i] + bracketsConfig[i+1];
+   
+    temp = RegExp(tt, 'g');    
+    arr.push(temp);
+  }
+
+  let fl = 1;  
+  let iter = 0;    
+  while ( fl != 0) {   
+    let arrfl = []; 
+    fl = 0;
+    for (let i = 0; i< arr.length; i++){
+    
+        if (arr[i].test(str)){ 
+          str = str.replace(arr[i], '');
+          arrfl[i] = 1;
+        }  
+        else
+          arrfl[i] = 0;
+
+    }
+    for (let i = 0; i<arrfl.length; i++){
+      fl = fl + arrfl[i];
+    }
+    console.log('fl = '+fl);
+    console.log(str.length);
+  }
+
+    console.log('результат str = ', str);
+    
+    return (str.length == 0)? true : false;
+  
 }
+
